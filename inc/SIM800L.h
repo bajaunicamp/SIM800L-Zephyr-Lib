9fa69a0024a340c8ab078fabfcab88dba2d69cca
lucasmnd <lucasrodriguesdemendonca@gmail.com>
2024-09-05 00:36:39 -0300

    Criei novas funções e comecei a documentar a biblioteca
    Criei uma função que envia uma mensagem ao servidor
    Comecei a documentar as funções e structs usando Doxygen

--- inc/SIM800L.h
+++ inc/SIM800L.h
@@ -3,21 +3,49 @@
 
 #include "zephyr/drivers/uart.h"
 
+/**
+ * @struct s_input_dev
+ * @brief Representa a conexão UART com o GPS
+ */
 struct s_input_dev{
+    /** Referência ao device da device tree que representa o canal UART sendo usado */
     const struct device *dev;
+    /** Buffer no qual serão registradas mensagens recebidas através de callback*/
     char *buffer;
+    /** Número de caracteres do buffer que armazena as mensagens recebidas por callback*/
     int buffer_size;
 };
 
+/**
+ * @struct s_server
+ * @brief Representa a conexão UART com o SIM800L
+ */
 struct s_server{
+    /** Referência ao device da device tree que representa o canal UART sendo usado*/
     const struct device *dev;
+    /** Buffer no qual serão registradas mensagens recebidas através de callback*/
     char *buffer;
+    /** Número de caracteres do buffer que armazena as mensagens recebidas por callback*/
     int buffer_size;
+    /** Booleano que registra se há conexão com o servidor*/
+    bool connected;
 };
-
+// TODO: Criar as funções que lidam com os dados vindo do GPS
 int init_gps_in();
-int init_server();
 void gps_in_callback(const struct device *dev, struct uart_event *evt, void *user_data);
+
+/**
+ * @brief Estabelece o canal de comunicação UART do STM com o SIM800L e estabelece uma conexão entre o SIM800L e o servidor
+ */
+int init_server();
+
+// TODO: Criar callback do servidor para poder ler as respostas
 void server_callback(const struct device *dev, struct uart_event *evt, void *user_data);
+
+/**
+ * @brief Envia uma mensagem ao servidor
+ * @param message Mensagem a ser enviada ao servidor
+ */
+void send_server(char *message);
 
 #endif
--- src/SIM800L.c
+++ src/SIM800L.c
@@ -7,65 +7,72 @@
 struct s_server server;
 struct s_input_dev gps_in;
 
-int init_gps_in(){
-
-}
-
 int init_server(){
+    // Inicializando a comunicação UART
     server.dev = DEVICE_DT_GET(DT_ALIAS(sim800l));
     server.buffer_size = 50;
     server.buffer = malloc(50*sizeof(char));
+    server.connected = false;
     int ret = uart_callback_set(server.dev, server_callback, NULL);
     if(ret) return ret;
 
-    char message[100];
+    char temp[100];
 
     // TODO: substituir o delay por uma verificação de conexão
     k_msleep(10000);
 
     // Select TCIP Application Mode
-    sprintf(message, "AT+CIPMODE=0\r\n");
-    uart_tx(server.dev, message, strlen(message), 100);
+    sprintf(temp, "AT+CIPMODE=0\r\n");
+    uart_tx(server.dev, temp, strlen(temp), 100);
     k_msleep(100);
 
     // Startup single IP connection
-	sprintf(message, "AT+CIPMUX=0\r\n");
-    uart_tx(server.dev, message, strlen(message), 100);
+	sprintf(temp, "AT+CIPMUX=0\r\n");
+    uart_tx(server.dev, temp, strlen(temp), 100);
     k_msleep(100);
 
 	// Attach from GPRS Service
-	sprintf(message, "AT+CGATT=1\r\n");
-    uart_tx(server.dev, message, strlen(message), 100);
+	sprintf(temp, "AT+CGATT=1\r\n");
+    uart_tx(server.dev, temp, strlen(temp), 100);
     k_msleep(100);
 
 	// Start task and set APN
-	sprintf(message, "AT+CSTT=\"timbrasil.br\",\"tim\",\"tim\"\r\n");
-    uart_tx(server.dev, message, strlen(message), 100);
+	sprintf(temp, "AT+CSTT=\"timbrasil.br\",\"tim\",\"tim\"\r\n");
+    uart_tx(server.dev, temp, strlen(temp), 100);
     k_msleep(10000);
 
 	// Bring up wireless connection with GPRS  or CSD
-	sprintf(message, "AT+CIICR\r\n");
-    uart_tx(server.dev, message, strlen(message), 100);
+	sprintf(temp, "AT+CIICR\r\n");
+    uart_tx(server.dev, temp, strlen(temp), 100);
     k_msleep(100);
 
 	// Get Local IP address
-	sprintf(message, "AT+CIFSR\r\n");
-    uart_tx(server.dev, message, strlen(message), 100);
+	sprintf(temp, "AT+CIFSR\r\n");
+    uart_tx(server.dev, temp, strlen(temp), 100);
     k_msleep(100);
-
 
 	// Start up TCP connection
-	sprintf(message, "AT+CIPSTART=\"TCP\",\"tcp://0.tcp.sa.ngrok.io\",\"00000\"\r\n");
-    uart_tx(server.dev, message, strlen(message), 100);
+	sprintf(temp, "AT+CIPSTART=\"TCP\",\"tcp://0.tcp.sa.ngrok.io\",\"00000\"\r\n");
+    uart_tx(server.dev, temp, strlen(temp), 100);
+    server.connected = true;
     k_msleep(100);
 
-	// TODO: Enviar mensagem pro servidor que é o baja que tá conectando
-	sendServidor("[CONNECTION] Baja");
-	painelStep(5);
+	// Enviar mensagem pro servidor que é o baja que tá conectando
+    send_server("[CONNECTION] Baja");
 
     return 0;
 }
 
+void send_server(char *message){
+    char temp[100];
 
+    sprintf(temp, "AT+CIPSEND=%d\r\n", strlen(message));
+    uart_tx(server.dev, temp, strlen(temp), 100);
+    k_msleep(100);
+
+    sprintf(temp, "%s\r\n", message);
+    uart_tx(server.dev, temp, strlen(temp));
+    k_msleep(100);
+}
 
 
