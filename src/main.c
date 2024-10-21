07243afeb9bf0a195e7f800035f22eece4b7a7eb
lucasmnd <lucasrodriguesdemendonca@gmail.com>
2024-10-20 23:24:22 -0300

    Removi partes desnecessárias da main

--- src/main.c
+++ src/main.c
@@ -1,5 +1,3 @@
-// Esse programa de expemplo printa "Hello World!" no LOG no momento em que o led pisca
-
 #include "zephyr/device.h"
 #include "zephyr/sys/printk.h"
 #include "zephyr/logging/log.h"
@@ -9,8 +7,6 @@
 
 
 LOG_MODULE_REGISTER();
-
-const struct gpio_dt_spec *const led = &(const struct gpio_dt_spec)GPIO_DT_SPEC_GET(DT_NODELABEL(led0), gpios);
 
 int main(){
   init_server(11547, TIM);
